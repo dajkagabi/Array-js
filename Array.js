07e3335bb73e8063c05940a874c1ca7a991925e9
dajkagabi <70899647+dajkagabi@users.noreply.github.com>
2023-01-08 21:00:29 +0100

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,13 @@
+Create an Array
+
+One way we can create an array is to use an array literal. An array literal creates an array by wrapping items in square brackets []. Remember from the previous exercise, arrays can store any data type — we can have an array that holds all the same data types or an array that holds different data types. 
+
+Let’s take a closer look at the syntax in the array example:
+
+    The array is represented by the square brackets [] and the content inside.
+    Each content item inside an array is called an element.
+    There are three different elements inside the array.
+    Each element inside the array is a different data type
+
+const hobbies =  ['first', 'second', 'five'];
+console.log(hobbies);
